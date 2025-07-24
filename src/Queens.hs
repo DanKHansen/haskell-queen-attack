652569938db50d05cfbf910b2f798472cfd687a6
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-24 10:41:25 +0200

    solution

--- src/Queens.hs
+++ src/Queens.hs
@@ -1,7 +1,12 @@
 module Queens (boardString, canAttack) where
 
 boardString :: Maybe (Int, Int) -> Maybe (Int, Int) -> String
-boardString white black = error "You need to implement this function."
+boardString qW qB = unlines [unwords [board x y | y <- [0 .. 7]] | x <- [0 .. 7]]
+  where
+    board r c
+      | Just (r, c) == qW = "W"
+      | Just (r, c) == qB = "B"
+      | otherwise = "_"
 
 canAttack :: (Int, Int) -> (Int, Int) -> Bool
-canAttack queenA queenB = error "You need to implement this function."
+canAttack qW qB = fst qW == fst qB || snd qW == snd qB || abs (fst qW - fst qB) == abs (snd qW - snd qB)
